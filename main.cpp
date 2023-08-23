b768832ebec9f78427d73a4317376710ac79dfb5
Luis Muñoz <luisd.munoz@udea.edu.co>
2023-08-22 20:03:17 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int opcion=1;
+
+
+    while(opcion != 0){
+        cout<<"********** Menu Practica #1 **********"<<endl;
+        cout <<"Ingrese el numero del problema que desea de los 17 problemas que se tiene. "<<endl;
+        cout<<"Ingrese cero para salir."<<endl;
+        cin>> opcion;
+
+        switch(opcion){
+        case 0:
+            cout<<endl;
+            break;
+
+        case 1:
+            cout<<"Problema #1."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 2:
+            cout<<"Problema #2."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 3:
+            cout<<"Problema #3."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 4:
+            cout<<"Problema #4."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 5:
+            cout<<"Problema #5."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 6:
+            cout<<"Problema #6."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 7:
+            cout<<"Problema #7."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 8:
+            cout<<"Problema #8."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 9:
+            cout<<"Problema #9."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 10:
+            cout<<"Problema #10."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 11:
+            cout<<"Problema #11."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 12:
+            cout<<"Problema #12."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 13:
+            cout<<"Problema #13."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 14:
+            cout<<"Problema #14."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 15:
+            cout<<"Problema #15."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 16:
+            cout<<"Problema #16."<<endl;
+
+            cout<<endl;
+            break;
+
+        case 17:
+            cout<<"Problema #17."<<endl;
+
+            cout<<endl;
+            break;
+
+        default:
+            cout<<"Se ingreso un valor invalido."<<endl;
+
+            cout<<endl;
+            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }   // LLave del switch case.
+    } //Llave del while grande.
+    cout<<"El programa a finalizado."<<endl;
+    return 0;   }
